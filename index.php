ab9641c17fc44952550bbaf20e278b893d77e968
zll600 <3400692417@qq.com>
2024-08-01 22:35:56 +0800

    add index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+    <h1>
+        <?php
+        echo "hello world";
+        ?>
+    </h1>
+</body>
+</html>
