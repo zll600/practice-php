79bc8505201a4b7b15b1243460356a004cc1dd69
zll600 <3400692417@qq.com>
2024-08-01 22:50:58 +0800

    add if conditions

--- index.php
+++ index.php
@@ -5,10 +5,17 @@
     <title>Demo</title>
 </head>
 <body>
+        <?php
+            $name = "books";
+            $read = true;
+
+            if ($read) {
+                $message = "message";
+            }
+        ?>
     <h1>
         <?php
-            $greeting = "Hello";
-            echo "$greeting World";
+            echo "$message is our message";
         ?>
     </h1>
 </body>
