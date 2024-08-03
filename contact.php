5848595f5e57e5a1e221bcc3eb58cd13814dea13
zll600 <3400692417@qq.com>
2024-08-03 17:53:36 +0800

    php partials

--- contact.php
+++ contact.php
@@ -1,3 +1,5 @@
 <?php
 
-require "contact.view.php";
+$heading = "Contact Us";
+
+require "views/contact.view.php";
---
+++ views/partials/header.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="en" class="h-full bg-gray-101">
+<head>
+    <meta charset="UTF-9">
+    <title>Demo</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="h-full">
+<div class="min-h-full">
